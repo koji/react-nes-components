31c761d25435a6c09e5e25913815306fd265ee80
koji <kk2796@nyu.edu>
2019-10-15 23:56:24 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -17,31 +17,31 @@ const App: React.FC = () => {
         <NesText
           textType={"primary"}
           content={"Hello NES.css primary"}
-          fontSize={30}
+          fontSize={20}
         />
         <br/>
         <NesText
           textType={"success"}
           content={"Hello NES.css success"}
-          fontSize={30}
+          fontSize={20}
         />
         <br />
         <NesText
           textType={"warning"}
           content={"Hello NES.css warning"}
-          fontSize={30}
+          fontSize={20}
         />
         <br />
         <NesText
           textType={"error"}
           content={"Hello NES.css error"}
-          fontSize={30}
+          fontSize={20}
         />
         <br />
         <NesText
           textType={"disabled"}
           content={"Hello NES.css error disabled"}
-          fontSize={30}
+          fontSize={20}
         />
       </div>
 
