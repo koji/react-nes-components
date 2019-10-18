4ff83c05e69fbda754370e2b155c560428296a8c
koji <kk2796@nyu.edu>
2019-10-18 00:19:04 -0400

    added other icons

--- src/Icons/index.tsx
+++ src/Icons/index.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import "../../node_modules/nes.css/css/nes.css";
 
-type Props = {};
+type Props = {
+  iconType: "close" | "trophy" | "coin";
+  iconSize: "large" | "medium" | "small";
+};
 
 export class NesIcon extends React.Component<Props> {
   render() {
-    return <div></div>;
+    const { iconType, iconSize } = this.props;
+    const iconClass = `nes-icon ${iconType} is-${iconSize}`;
+    return (
+      <i className={iconClass}></i>
+    );
   }
 }
