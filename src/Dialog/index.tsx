2f861230387231cc6987daa7e9156005ed71ae4b
koji <kk2796@nyu.edu>
2019-10-19 22:32:37 -0400

    created select base

--- src/Dialog/index.tsx
+++ src/Dialog/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import 'nes.css';
+import "../../node_modules/nes.css/css/nes.css";
 
 type Props = {};
 
--- src/Dropdown/index.tsx
+++ src/Dropdown/index.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
-import 'nes.css';
+import "../../node_modules/nes.css/css/nes.css";
 
-type Props = {};
+type Props = {
+  label?: string;
+  options: Array<string|number>;
+  value: Array<string|number>;
+  selectType?: "success" | "warning" | "error";
+  darkMode?: boolean
+};
 
+// NES.css select
 export class NesDropdown extends React.Component<Props> {
   render() {
+    const {label, options, value, selectType, darkMode } = this.props;
+    const optionElms = options.map((option, i) =>
+      <option key={`option-${i}`} value={value[i]}>{option}</option>
+    );
+    const labelType = label ? `${selectType}_select` : `default_select`;
+    const selectId = selectType ? `${selectType}_select` : `default_select`;
+    let selectClass = selectType ? `nes-select is-${selectType}` : `nes-select`;
+    selectClass = darkMode ? `${selectClass} is-dark` : `${selectClass}`;
     return (
-      <div></div>
+      <React.Fragment>
+      {
+        label && (
+            <label htmlFor={labelType}>{label}</label>
+        )
+      }
+      <div className={selectClass}>
+        <select required id={selectId}>
+          {optionElms}
+        </select>
+      </div>
+      </React.Fragment>
     )
   }
 }
