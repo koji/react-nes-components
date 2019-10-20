f6ad50a5a532a2f7068260765982e71c12539876
koji <kk2796@nyu.edu>
2019-10-20 00:12:27 -0400

    update
    modified textarea

---
+++ src/stories/NesTextarea.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { NesTextarea } from "../Textarea";
+
+storiesOf("nes-components/NesTextarea", module)
+  .addDecorator(story => (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {story()}
+    </div>
+  ))
+  .addParameters({
+    props: {
+      propTablesExclude: [{ NesTextarea }]
+    }
+  })
+  .add("textarea", () => (
+    <NesTextarea
+     content={"This is textarea's sample!!!"}
+    />
+  ));
