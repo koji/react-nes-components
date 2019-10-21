7b5cdeaaced0c6696c72f6e0d7629b3ed9081e8f
koji <kk2796@nyu.edu>
2019-10-21 03:19:11 -0400

    added container

---
+++ src/stories/NesContainer.stories.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { NesContainer } from "../Container";
+
+storiesOf("nes-components/NesContainer", module)
+  .addDecorator(story => (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}
+    >
+      {story()}
+    </div>
+  ))
+  .addParameters({
+    props: {
+      propTablesExclude: [{ NesContainer }]
+    }
+  })
+  .add("Container", () => (
+    <NesContainer
+      content={"This is nes-components' container"}
+    />
+  ))
+  .add("Container_centered", () => (
+    <NesContainer
+      containerTitle={"nes-components"}
+      isCentered={true}
+      content={"This is nes-components' container"}
+    />
+  ))
+  .add("Container_dark", () => (
+    <NesContainer
+      containerTitle={"nes-components"}
+      isCentered={false}
+      isRounded={true}
+      darkMode={true}
+      content={"This is nes-components' container"}
+    />
+  ))
+  .add("Container with title", () => (
+    <NesContainer
+      containerTitle={"nes-components"}
+      isCentered={true}
+      isRounded={true}
+      content={"This is nes-components' container"}
+    />
+  ));
