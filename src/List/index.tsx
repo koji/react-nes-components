b089f0a9925e7e62e5bb2d8e34ed627ea07391be
koji <kk2796@nyu.edu>
2019-10-27 01:07:42 -0400

    update checkbox and radio

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export class NesList extends React.Component<Props> {
   render() {
     const { items, isDisc } = this.props;
-    const itemType: string = "nes-list";
+    const itemType = "nes-list";
     const classType = isDisc ? `${itemType} is-disc` : `${itemType} is-circle`;
     // create li items
     const listItems = items.map((item, i) =>
--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -4,24 +4,28 @@ import "../../node_modules/nes.css/css/nes.css";
 // buttonType undefined --> normal
 type Props = {
   darkMode?: boolean;
-  labels: string[];
+  items: string[];
 };
 
 export class NesRadio extends React.Component<Props> {
   render() {
-    const { darkMode, labels } = this.props;
+    const { darkMode, items } = this.props;
     const classType = darkMode ? `nes-radio is-dark` : `nes-radio`;
-    return (
-      labels.map((label, index) => {
+    return items.map((item, index) => {
+      return (
         <label>
-          {index === 0 ? (
-            <input type="radio" className={classType} name="answer" checked />
-          ) : (
-            <input type="radio" className={classType} name="answer"/>
-          )}
-          <span>{label}</span>
-        </label>;
-      })
-    );
+          <input type="radio" className={classType} name="answer" />
+          <span>{item}</span>
+        </label>
+      );
+    });
   }
 }
+
+
+// ToDo
+// {index === 0 ? (
+//             <input type="radio" className={classType} name="answer" />
+//           ) : (
+//             <input type="radio" className={classType} name="answer" /> */}
+// )}
