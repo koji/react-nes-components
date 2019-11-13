69d076f581a7b4f5708dddf201cd63543aa6e3af
koji <kk2796@nyu.edu>
2019-11-13 02:01:48 -0500

    Update Badge.test.tsx

--- src/__tests__/Badge/Badge.test.tsx
+++ src/__tests__/Badge/Badge.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-// import { render, cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import { NesBadge } from '../../Badge';
 // import 'jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
 
+afterEach(cleanup);
 describe('<NesBadge>', () => {
-  // it('renders without crashing', () => {
-  //   const { getAllByDisplayValue } = render(
-  //     <NesBadge badgeType={['primary']} contents={['nes-components']} />
-  //   );
-  //   expect(getAllByDisplayValue).toBe('nes-components');
-  // });
+  it('renders without crashing', () => {
+    const { queryByText } = render(
+      <NesBadge badgeType={['primary']} contents={['nes-components']} />
+    );
+    expect(queryByText('nes-components')).toBeTruthy();
+  });
 
   it('renders without crashing', () => {
     const tree = renderer
